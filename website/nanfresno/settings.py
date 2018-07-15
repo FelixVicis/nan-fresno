790c224a1f705f6e938dee2160594af44aca10e7
spriggsj <jasonspriggs73@gmail.com>
2018-07-15 00:00:19 -0700

    rebuild of marketing

--- website/nanfresno/settings.py
+++ website/nanfresno/settings.py
@@ -34,8 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'marketing',
     'people',
-    'hello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
