fb55f6274960f41e2b990d49cb191d46e0d2f36b
Allen Mills <Mills.Allen.James@gmail.com>
2018-07-15 18:09:36 -0700

    remove duplicate import

--- website/nanfresno/urls.py
+++ website/nanfresno/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf.urls import url, include
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
