083f7fec73ca6dec538f16510a0c8195ad46d119
spriggsj <jasonspriggs73@gmail.com>
2018-07-16 17:14:52 -0700

    Uncommented UUID and added semicolon to &copy;

--- website/people/models.py
+++ website/people/models.py
@@ -14,7 +14,7 @@ def upload_location(instance, filename):
 
 
 class People(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     lastName = models.CharField(max_length=200)
     firstName = models.CharField(max_length=200, null=True, blank=True)
     city = models.CharField(max_length=200, null=True, blank=True)
