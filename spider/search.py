93fb0c8e451c70f01a23ec1eb0c8f234a086ec66
Allen Mills <Mills.Allen.James@gmail.com>
2018-07-28 14:27:52 -0700

    feature: add search

---
+++ spider/search.py
@@ -0,0 +1,11 @@
+import googlesearch as google
+
+
+def websites(query, start=0, stop=None, per_page=10):
+    return google.search(query, start=start, stop=stop, num=per_page)
+
+
+def images(query, start=0, stop=None, per_page=10):
+    return google.search_images(query, start=start, stop=stop, num=per_page)
+
+# http://editllc.github.io/client-house-psychiatric/staff.html
